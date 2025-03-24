017c6aeaf2d3e31a852d0db65264522b1dba322c
TahaAyati <TahaAyati2007@gmail.com>
2025-03-25 00:31:39 +0330

    feat: create essential entities for the project

--- DataLayer/obj/Debug/net5.0/DataLayer.AssemblyInfo.cs
+++ DataLayer/obj/Debug/net5.0/DataLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42fdea6cfe723ce40d774f0016fc766d4cacda28")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
